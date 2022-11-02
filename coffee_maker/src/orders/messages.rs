fed28329246ca424b893f3ebeebd095dcbfba17f
Nicolas Zulaica <nzulaica@fi.uba.ar>
2022-11-02 17:52:29 -0300

    coffe_maker's actor's messages

--- coffee_maker/src/orders/messages.rs
+++ coffee_maker/src/orders/messages.rs
@@ -1,5 +1,31 @@
 use actix::prelude::*;
 
+pub enum Order {
+    UsePoints(usize),
+    FillPoints(usize),
+}
+
+// Order Taker
 #[derive(Message)]
 #[rtype(result = "()")]
 pub struct TakeOrders(pub String);
+
+// Order Store
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct NoMoreOrders;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct StoreOrder(pub Order);
+
+/*
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct AskOrder(pub Addr<OrderHandler>);
+*/
+
+// Order Handler
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct HandleOrder(pub Order);
