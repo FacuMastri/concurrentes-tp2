b16454e16fa76623c80c10ea989f7635ba1d6a52
Santiago Tissoni <santi.tissoni@gmail.com>
2022-11-13 03:08:41 -0300

    2PC

---
+++ core_server/src/message.rs
@@ -0,0 +1,66 @@
+use crate::transaction_state::TransactionState;
+
+#[derive(Debug)]
+pub struct Message {
+    pub transaction_state: TransactionState,
+    pub transaction_id: u64,
+    pub order_type: u8,
+    pub client_id: u64,
+    pub points: u64,
+}
+
+impl Message {
+    pub fn new(transaction_id: u64, order_type: u8, client_id: u64, points: u64) -> Self {
+        Message {
+            transaction_state: TransactionState::Prepare, // si la transaccion es nueva empieza en estado prepare
+            transaction_id,
+            order_type,
+            client_id,
+            points,
+        }
+    }
+}
+
+pub fn be_byte_buffer_to_u64(buffer: &[u8]) -> u64 {
+    let mut mask_buffer = [0u8; 8];
+    mask_buffer.copy_from_slice(&buffer[0..8]);
+    u64::from_be_bytes(mask_buffer)
+}
+
+// se entiende en big endian
+impl From<Vec<u8>> for Message {
+    fn from(v: Vec<u8>) -> Self {
+        Message {
+            transaction_state: v[0].into(),
+            transaction_id: be_byte_buffer_to_u64(&v[1..9]),
+            order_type: v[9].into(),
+            client_id: be_byte_buffer_to_u64(&v[10..18]),
+            points: be_byte_buffer_to_u64(&v[18..]),
+        }
+    }
+}
+
+impl From<Message> for Vec<u8> {
+    fn from(data: Message) -> Self {
+        let mut res = vec![data.transaction_state.into()];
+        res.extend_from_slice(&data.transaction_id.to_be_bytes());
+        res.extend_from_slice(&data.order_type.to_be_bytes());
+        res.extend_from_slice(&data.client_id.to_be_bytes());
+        res.extend_from_slice(&data.points.to_be_bytes());
+        res
+    }
+}
+
+mod tests {
+
+    #[test]
+    fn test_message() {
+        use crate::message::Message;
+        let message = Message::new(12, 1, 98, 100);
+        println!("Message: {:?}", message);
+        let bytes: Vec<u8> = message.into();
+        println!("{:?}", bytes);
+        let message: Message = bytes.into();
+        println!("Message: {:?}", message);
+    }
+}
--- core_server/src/points.rs
+++ core_server/src/points.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 #[derive(Debug)]
 #[allow(dead_code)] // FIXME
 pub struct Points {
-    points: HashMap<String, i32>,
+    points: HashMap<u64, u64>,
 }
 
 #[allow(dead_code)] // FIXME
@@ -13,23 +13,25 @@ impl Points {
         Points { points }
     }
 
-    pub fn add_points(&mut self, client_id: String, points: i32) {
+    pub fn add_points(&mut self, client_id: u64, points: u64) {
         *self.points.entry(client_id).or_insert(0) += points;
     }
 
-    pub fn remove_points(&mut self, client_id: String, points: i32) -> Result<i32, ()> {
+    pub fn remove_points(&mut self, client_id: u64, required_points: u64) -> Result<u64, ()> {
         if !self.points.contains_key(&client_id) {
             return Err(());
         }
-        let actual_points = self.points.get(&client_id).unwrap();
-        if actual_points - points < 0 {
+        let actual_points = *self.points.get(&client_id).unwrap();
+        if actual_points < required_points {
             return Err(());
         }
-        self.points.entry(client_id).and_modify(|e| *e -= points);
-        Ok(points)
+        self.points
+            .entry(client_id)
+            .and_modify(|e| *e -= required_points);
+        Ok(required_points)
     }
 
-    pub fn get_points(&self, client_id: String) -> Result<i32, ()> {
+    pub fn get_points(&self, client_id: u64) -> Result<u64, ()> {
         if !self.points.contains_key(&client_id) {
             Err(())
         } else {
@@ -46,15 +48,15 @@ mod test {
     #[test]
     fn add_points() {
         let mut points = Points::new();
-        points.add_points("John".to_string(), 100);
-        assert_eq!(100, points.get_points("John".to_string()).unwrap());
+        points.add_points(42, 100);
+        assert_eq!(100, points.get_points(42).unwrap());
     }
     #[test]
     fn remove_points() {
         let mut points = Points::new();
-        points.add_points("John".to_string(), 100);
-        let points_removed = points.remove_points("John".to_string(), 30).unwrap();
+        points.add_points(42, 100);
+        let points_removed = points.remove_points(42, 30).unwrap();
         assert_eq!(30, points_removed);
-        assert_eq!(70, points.get_points("John".to_string()).unwrap());
+        assert_eq!(70, points.get_points(42).unwrap());
     }
 }
