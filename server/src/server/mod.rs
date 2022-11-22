6ad20dc9fee638a510db493e66a4e6b71054b36f
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-11-22 01:41:59 -0300

    Add 2 use case test
    - Test server should restore points when a USE POINTS order fails
    - Test server should reserve points while in progress of a USE POINTS order

--- server/src/server/mod.rs
+++ server/src/server/mod.rs
@@ -384,7 +384,6 @@ impl Server {
 }
 
 #[cfg(test)]
-// All the following tests require that the success chance from the coffee machine is 100%.
 mod tests {
     use crate::server::message::{send_message_to, SyncRequest, SYNC};
     use points::{parse_addr, ControlMessage, CONTROL_MESSAGE};
@@ -529,6 +528,7 @@ mod tests {
 
         let mut new_server = Command::new("cargo")
             .args(["run", "--bin", "server", "9002", "9000"])
+            .stdout(Stdio::null())
             .spawn()
             .expect("Failed to start server");
         thread::sleep(Duration::from_millis(1000));
@@ -573,6 +573,7 @@ mod tests {
 
         let mut server_3 = Command::new("cargo")
             .args(["run", "--bin", "server", "9002", "9000"])
+            .stdout(Stdio::null())
             .spawn()
             .expect("Failed to start server");
 
@@ -930,5 +931,184 @@ mod tests {
         assert_eq!(synced_points_server_1, expected_result);
         assert_eq!(synced_points_server_2, expected_result);
         assert_eq!(synced_points_server_3, expected_result);
+    }
+
+    #[test]
+    #[serial]
+    fn server_should_restore_points_when_use_order_fails() {
+        let expected_result = json!({
+        "points": {
+            "1": {
+                "points": [25, 0],
+                "transaction": null,
+            },
+            }
+        })
+        .to_string();
+        let mut server_1 = Command::new("cargo")
+            .args(["run", "--bin", "server", "9000"])
+            .stdout(Stdio::null())
+            .spawn()
+            .expect("Failed to start server");
+
+        // El sleep es para dar tiempo a buildear al tirar un cargo run
+        thread::sleep(Duration::from_millis(1000));
+
+        let mut server_2 = Command::new("cargo")
+            .args(["run", "--bin", "server", "9001", "9000"])
+            .stdout(Stdio::null())
+            .spawn()
+            .expect("Failed to start server");
+
+        // El sleep es para dar tiempo a buildear al tirar un cargo run
+        thread::sleep(Duration::from_millis(1000));
+
+        // Aplicamos una orden a los servidores que estan conectados
+        let mut coffee_maker = Command::new("cargo")
+            .current_dir("../")
+            .stdout(Stdio::null())
+            .args([
+                "run",
+                "--bin",
+                "coffee_maker",
+                "9000",
+                "assets/orders-3-test.csv",
+            ])
+            .spawn()
+            .expect("Failed to start coffee maker");
+        // Esperamos que la cafetera termine de procesar
+        coffee_maker.wait().unwrap();
+
+        // Le ponemos una orden de USE POINTS al servidor 9001, pero
+        // debe fallar al tener una success_chance = 0
+        let mut coffee_maker = Command::new("cargo")
+            .current_dir("../")
+            .stdout(Stdio::null())
+            .args([
+                "run",
+                "--bin",
+                "coffee_maker",
+                "9001",
+                "assets/orders-3-test-3.csv",
+                "0",
+            ])
+            .spawn()
+            .expect("Failed to start coffee maker");
+
+        coffee_maker.wait().unwrap();
+
+        let synced_points_server_1 =
+            send_message_to(SYNC, SyncRequest {}, &"localhost:9000".to_owned())
+                .expect("Failed to sync");
+        let synced_points_server_2 =
+            send_message_to(SYNC, SyncRequest {}, &"localhost:9001".to_owned())
+                .expect("Failed to sync");
+
+        server_1.kill().expect("Failed to kill server 1");
+        server_2.kill().expect("Failed to kill server 2");
+
+        assert_eq!(synced_points_server_1, expected_result);
+        assert_eq!(synced_points_server_2, expected_result);
+    }
+
+    #[test]
+    #[serial]
+    fn server_should_reserve_points_while_in_process_of_use_points_order() {
+        let expected_reserved_points = json!({
+        "points": {
+            "1": {
+                "points": [20, 5],
+                "transaction": null,
+            },
+            }
+        })
+        .to_string();
+
+        let expected_final_points = json!({
+        "points": {
+            "1": {
+                "points": [20, 0],
+                "transaction": null,
+            },
+            }
+        })
+        .to_string();
+
+        let mut server_1 = Command::new("cargo")
+            .args(["run", "--bin", "server", "9000"])
+            .stdout(Stdio::null())
+            .spawn()
+            .expect("Failed to start server");
+
+        // El sleep es para dar tiempo a buildear al tirar un cargo run
+        thread::sleep(Duration::from_millis(1000));
+
+        let mut server_2 = Command::new("cargo")
+            .args(["run", "--bin", "server", "9001", "9000"])
+            .stdout(Stdio::null())
+            .spawn()
+            .expect("Failed to start server");
+
+        // El sleep es para dar tiempo a buildear al tirar un cargo run
+        thread::sleep(Duration::from_millis(1000));
+
+        // Aplicamos una orden a los servidores que estan conectados
+        let mut coffee_maker = Command::new("cargo")
+            .current_dir("../")
+            .stdout(Stdio::null())
+            .args([
+                "run",
+                "--bin",
+                "coffee_maker",
+                "9000",
+                "assets/orders-3-test.csv",
+            ])
+            .spawn()
+            .expect("Failed to start coffee maker");
+        // Esperamos que la cafetera termine de procesar
+        coffee_maker.wait().unwrap();
+
+        // Le ponemos una orden de USE POINTS al servidor 9001, pero
+        // debe seguir al tener una success_chance = 1
+        let mut coffee_maker = Command::new("cargo")
+            .current_dir("../")
+            .stdout(Stdio::null())
+            .args([
+                "run",
+                "--bin",
+                "coffee_maker",
+                "9001",
+                "assets/orders-3-test-3.csv",
+                "1",
+            ])
+            .spawn()
+            .expect("Failed to start coffee maker");
+
+        thread::sleep(Duration::from_millis(1000));
+
+        let sync_reserved_points_server_1 =
+            send_message_to(SYNC, SyncRequest {}, &"localhost:9000".to_owned())
+                .expect("Failed to sync");
+
+        let sync_reserved_points_server_2 =
+            send_message_to(SYNC, SyncRequest {}, &"localhost:9001".to_owned())
+                .expect("Failed to sync");
+
+        coffee_maker.wait().unwrap();
+
+        let sync_final_points_server_1 =
+            send_message_to(SYNC, SyncRequest {}, &"localhost:9000".to_owned())
+                .expect("Failed to sync");
+        let sync_final_points_server_2 =
+            send_message_to(SYNC, SyncRequest {}, &"localhost:9001".to_owned())
+                .expect("Failed to sync");
+
+        server_1.kill().expect("Failed to kill server 1");
+        server_2.kill().expect("Failed to kill server 2");
+
+        assert_eq!(sync_reserved_points_server_1, expected_reserved_points);
+        assert_eq!(sync_reserved_points_server_2, expected_reserved_points);
+        assert_eq!(sync_final_points_server_1, expected_final_points);
+        assert_eq!(sync_final_points_server_2, expected_final_points);
     }
 }
